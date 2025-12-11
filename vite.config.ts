8312876a6a8d40691e39aae2ab6d9e8d89311bcb
Flik Jeong <jsh852@gmail.com>
2025-12-12 02:03:24 +0900

    chore: Add env files and update project settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), vanillaExtractPlugin()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@app': path.resolve(__dirname, './src/app'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@mocks': path.resolve(__dirname, './src/mocks'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
+});
